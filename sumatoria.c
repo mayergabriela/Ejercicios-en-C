7d6f67c30c922152b3fc48c503cdaa70fcf5fc73
Gabriela Mayer <mayergabriela@gmail.com>
2020-05-03 17:33:55 -0300

    sasasa

---
+++ sumatoria.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+int main (){
+    int numeroUno, numeroDos, resultado;
+    printf ("Ingrese dos numeros a sumar");
+    scanf("%d", & numeroUno);
+    resultado= numeroUno + numeroDos;
+    printf ("El resultado es", resultado)
+    system ("pause")
+    return 0;
+}
